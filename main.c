13f90d5f8929d13077458bd5acc9b30ed1be0e98
Grantrd <31990206+Grantrd@users.noreply.github.com>
2019-04-23 18:40:05 -0400

    Got reference voltage and printing fixed

--- main.c
+++ main.c
@@ -1,17 +1,28 @@
 #include <msp430.h>
 #include <stdio.h>
-void printer(int var)
+#include <stdbool.h>
+
+void printer(float var)
 {
-        printf("%d\n",var);
+    volatile float vint;
+    vint = var - (int)var;
+    if(vint == 0.00)
+    {
+        printf("%i\n", (int)var);
+    }
+    else
+    {
+        printf("%3.2f\n",var);
+    }
 }
 
-int adc(int p,int res, float volts)
+int adc(bool p,int res, float volts, char vb)
 {
-    volatile int AD;
+    volatile int resolution;
+    volatile float AD;
     P1SEL0 &= ~BIT4;
     P1SEL1 |= BIT4;
     int volt = (int)(volts * 10);
-
     if(volt == 12)
     {
         REFCTL0 |= REFVSEL_0;     /* Configure internal 1.2V reference. */
@@ -35,20 +46,23 @@ int adc(int p,int res, float volts)
         ADC12MCTL5 |= ADC12VRSEL_0;  // Configure internal 3.3V reference. */
         printer(volt);
     }
-    //ADC12MCTL5 |= ADC12VRSEL_1;
     REFCTL0  |= REFON;
 
     if(res == 8)
     {
         ADC12CTL2 &= ADC12RES_0; //resolution 8 bit
+        resolution = 255;
+
     }
     else if(res == 10)
     {
         ADC12CTL2 &= ADC12RES_1; //resolution resolution 10 bit
+        resolution = 1023;
     }
     else
     {
         ADC12CTL2 &= ADC12RES_2; //resolution 12 bit
+        resolution = 4095;
     }
 
     __delay_cycles(100);                    // delay to allow Ref to settle
@@ -61,12 +75,16 @@ int adc(int p,int res, float volts)
         volatile int i;
         for(;;)
         {
-            ADC12CTL0 &= ~ADC12SC;  // clear the start bit
-            ADC12CTL0 |= ADC12SC;       // Sampling and conversion start
+            ADC12CTL0 &= ~ADC12SC;                      // clear the start bit
+            ADC12CTL0 |= ADC12SC;                      // Sampling and conversion start
+            __delay_cycles(100);                      // delay to allow Ref to settle
+            AD = ADC12MEM5;                          // Read in results if conversion
             __delay_cycles(100);                    // delay to allow Ref to settle
-            AD = ADC12MEM5;      // Read in results if conversion
-            __delay_cycles(100);                    // delay to allow Ref to settle
-            printf("%d\n",AD);
+            if(vb == 'v')
+            {
+                AD = AD * (float)(volts/resolution);
+            }
+            printer(AD);
             i = 200;                          // SW Delay
             do i--;
             while(i != 0);
@@ -79,16 +97,18 @@ int adc(int p,int res, float volts)
         __delay_cycles(100);                    // delay to allow Ref to settle
         AD = ADC12MEM5;      // Read in results if conversion
         __delay_cycles(100);                    // delay to allow Ref to settle
-        printer(AD);
+        if(vb == 'v')
+        {
+            AD = AD * (float)(volts/resolution);
+        }
     }
     return AD;
 }
 
 
-void main(void) {
-    WDTCTL = WDTPW | WDTHOLD;                //Stop watchdog timer
-    PM5CTL0 &= ~LOCKLPM5;                   //Disable the GPIO power-on default high-impedance mode
-    adc(1, 12, 3.3);                      //adc(print, resolution, internal voltage reference, )
+void main(void)
+    {
+        WDTCTL = WDTPW | WDTHOLD;                //Stop watchdog timer
+        PM5CTL0 &= ~LOCKLPM5;                   //Disable the GPIO power-on default high-impedance mode
+        adc(true, 12, 1.2, 'b');                      //adc(print, resolution, internal voltage reference, )
     }
-
-
